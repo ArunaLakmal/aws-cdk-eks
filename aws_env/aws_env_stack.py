534e0e7629312a70d5e1ee03e5ff309f05677fea
ArunaLakmal <arunarandunu@gmail.com>
2022-03-18 14:44:36 +0100

    basic vpc

--- aws_env/aws_env_stack.py
+++ aws_env/aws_env_stack.py
@@ -1,7 +1,9 @@
+from unicodedata import name
 from aws_cdk import (
     # Duration,
+    CfnOutput,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2,
 )
 from constructs import Construct
 
@@ -10,10 +12,12 @@ class AwsEnvStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsEnvQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        vpc = ec2.Vpc(self, "TCVPC",
+            vpc_name="TCVPC",
+            cidr="172.16.0.0/16",
+            max_azs=2,
+            subnet_configuration=[ec2.SubnetConfiguration(cidr_mask=24,subnet_type=ec2.SubnetType.PUBLIC,name="Public"),
+            ec2.SubnetConfiguration(cidr_mask=24,subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT,name="Private")],
+            nat_gateways=2
+        )
+        CfnOutput(self, "Output", value=vpc.vpc_id)
