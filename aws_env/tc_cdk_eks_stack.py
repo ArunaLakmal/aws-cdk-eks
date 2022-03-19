c3dff0561d88a773d6d1d4143bc6e9acd12879c2
ArunaLakmal <arunarandunu@gmail.com>
2022-03-19 22:12:08 +0100

    UserMap

--- aws_env/tc_cdk_eks_stack.py
+++ aws_env/tc_cdk_eks_stack.py
@@ -3,7 +3,8 @@ from aws_cdk import (
     Stack,
     # aws_sqs as sqs,
     aws_eks as eks,
-    aws_ec2 as ec2
+    aws_ec2 as ec2,
+    aws_iam as iam
 )
 from constructs import Construct
 
@@ -20,3 +21,6 @@ class TcCdkEksStack(Stack):
             vpc=vpc,
             default_capacity_instance=ec2.InstanceType("t3.medium")
         )
+
+        admin_user = iam.User.from_user_attributes(self, "eks_test", user_arn="arn:aws:iam::123456789:user/eks_test")
+        cluster.aws_auth.add_user_mapping(admin_user, groups=["system:masters"])
