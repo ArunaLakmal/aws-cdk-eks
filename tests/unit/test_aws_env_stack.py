5d1b9c5bf57b7d39fb7d3e6dafd5e9d285b0dfcc
ArunaLakmal <arunarandunu@gmail.com>
2022-03-20 22:41:03 +0100

    check 1 eks custom cluster

--- tests/unit/test_aws_env_stack.py
+++ tests/unit/test_aws_env_stack.py
@@ -1,22 +1,15 @@
-import resource
-import aws_cdk as core
+import aws_cdk as cdk
 import aws_cdk.assertions as assertions
 
 from aws_env.aws_env_stack import AwsEnvStack
 from aws_env.tc_cdk_eks_stack import TcCdkEksStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in aws_env/aws_env_stack.py
-
 def test_aws_eks_cluster_created():
-    app = core.App()
-    stack = AwsEnvStack(app, "aws-env")
-    template = assertions.Template.from_stack(stack)
+    app = cdk.App()
+    infraStack = cdk.Stack(app, "AwsEnvStack")
+    vpc = cdk.aws_ec2.Vpc(infraStack, "TCVPC")
 
-    # eksstack = TcCdkEksStack(app, "TcCdkEksStack", vpc=stack.main_vpc)
-    # ekstemplate = assertions.Template.from_stack(eksstack)
+    eksstack = TcCdkEksStack(app, "TcCdkEksStack", vpc=vpc)
+    ekstemplate = assertions.Template.from_stack(eksstack)
 
-    template.resource_count_is("AWS::EKS::Cluster",1)
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    ekstemplate.resource_count_is("Custom::AWSCDK-EKS-Cluster",1)
