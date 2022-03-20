e465aaad8db433cf19b96b20bfe32fdd6bf1d201
ArunaLakmal <arunarandunu@gmail.com>
2022-03-20 21:59:35 +0100

    updated tests

--- tests/unit/test_aws_env_stack.py
+++ tests/unit/test_aws_env_stack.py
@@ -1,3 +1,4 @@
+import resource
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
@@ -6,15 +7,16 @@ from aws_env.tc_cdk_eks_stack import TcCdkEksStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in aws_env/aws_env_stack.py
-def test_sqs_queue_created():
+
+def test_aws_eks_cluster_created():
     app = core.App()
     stack = AwsEnvStack(app, "aws-env")
     template = assertions.Template.from_stack(stack)
-    
-    eksstack = TcCdkEksStack(app, "aws-env")
-    ekstemplate = assertions.Template.from_stack(eksstack)
 
-    ekstemplate.resource_count_is("AWS::EKS::Cluster",1)
+    # eksstack = TcCdkEksStack(app, "TcCdkEksStack", vpc=stack.main_vpc)
+    # ekstemplate = assertions.Template.from_stack(eksstack)
+
+    template.resource_count_is("AWS::EKS::Cluster",1)
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
 #     })
