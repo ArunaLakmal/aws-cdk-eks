1fdfb66404a6403323cdc97e67ff89ed68d55cdf
Aruna Lakmal <arunarandunu@gmail.com>
2022-03-22 21:12:27 +0100

    aws_auth and tests updates (#1)
    * vpc instance change
    
    * cleanup
    
    * Updated cluster name and pytest update with cluster count
    
    * updated tests
    
    * check 1 eks custom cluster

--- tests/unit/test_aws_env_stack.py
+++ tests/unit/test_aws_env_stack.py
@@ -1,15 +1,15 @@
-import aws_cdk as core
+import aws_cdk as cdk
 import aws_cdk.assertions as assertions
 
 from aws_env.aws_env_stack import AwsEnvStack
+from aws_env.tc_cdk_eks_stack import TcCdkEksStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in aws_env/aws_env_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = AwsEnvStack(app, "aws-env")
-    template = assertions.Template.from_stack(stack)
+def test_aws_eks_cluster_created():
+    app = cdk.App()
+    infraStack = cdk.Stack(app, "AwsEnvStack")
+    vpc = cdk.aws_ec2.Vpc(infraStack, "TCVPC")
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    eksstack = TcCdkEksStack(app, "TcCdkEksStack", vpc=vpc)
+    ekstemplate = assertions.Template.from_stack(eksstack)
+
+    ekstemplate.resource_count_is("Custom::AWSCDK-EKS-Cluster",1)
